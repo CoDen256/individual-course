b987a403719fdc6dabcb7a9601eca696831566fd
Denys Chernyshov <den.blackshov@gmail.com>
2024-02-14 21:47:50 +0100

    Add more flexibility

--- execution/compilation/src/Answers.java
+++ execution/compilation/src/Answers.java
@@ -1,4 +1,4 @@
-public class Answers2 {
+public class Answers {
 
     String A = "2"; // Output of the program
     String B = "4: iadd"; // javap -c output. Find a bytecode line, where integers are added. What instruction(or op-code is used)
--- execution/compilation/test/AnswersTest.java
+++ execution/compilation/test/AnswersTest.java
@@ -1,26 +1,17 @@
 import static utils.AnswerCheckUtils.question;
-import static utils.AnswerCheckUtils.verifyCorrectAnswer;
 
-import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
-import org.junit.jupiter.params.provider.MethodSource;
-import utils.Question;
+import utils.AnswerCheckerTestTemplate;
 import java.util.stream.Stream;
 
-public class AnswersTest {
-
-  @ParameterizedTest
-  @MethodSource("questions")
-  void answers(Question expected) throws NoSuchFieldException, IllegalAccessException, ClassNotFoundException {
-    verifyCorrectAnswer(expected);
-  }
-
-  public static Stream<Arguments> questions(){
-    return Stream.of(
-           question("A", "Mg=="),
-           question("B", "NDogaWFkZA=="),
-           question("C", "NDogaWFkZA=="),
-           question("D", "NDogaWFkZA==")
-    );
-  }
+public class AnswersTest extends AnswerCheckerTestTemplate {
+    @Override
+    public Stream<Arguments> questions() {
+        return Stream.of(
+                question("A", "Mg=="),
+                question("B", "NDogaWFkZA=="),
+                question("C", "NDogaWFkZA=="),
+                question("D", "NDogaWFkZA==")
+        );
+    }
 }
