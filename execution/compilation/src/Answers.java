d5134838916c4acc374fcce11a2cfbcb70f12daf
Denys Chernyshov <den.blackshov@gmail.com>
2024-02-14 21:36:26 +0100

    Finish answers checker

--- execution/compilation/src/Answers.java
+++ execution/compilation/src/Answers.java
@@ -3,5 +3,5 @@ public class Answers {
     String A = "2"; // Output of the program
     String B = "4: iadd"; // javap -c output. Find a bytecode line, where integers are added. What instruction(or op-code is used)
     String C = ""; //
-
+    String D = "<Your answer goes here>";
 }
